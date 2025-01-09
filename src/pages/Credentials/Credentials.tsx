cb29ed3bb8220425dacfe73e25090cf793ebfad3
james-hinton <james.hinton@live.co.uk>
2025-01-09 15:03:36 +0000

    feat: implement tabbed interface for Credentials component to enhance user navigation

--- src/pages/Credentials/Credentials.tsx
+++ src/pages/Credentials/Credentials.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+
+import { SiAmazons3 } from 'react-icons/si';
+import { TbApi } from 'react-icons/tb';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+
 import cloudIcon from '@/assets/icons/cloud.svg';
 
 import { DataHub } from '../DataHub/DataHub';
@@ -6,6 +12,8 @@ import { S3 } from '../S3/S3';
 import './Credentials.scss';
 
 export const Credentials = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <div className="content-page credentials">
       <div className="header">
@@ -21,15 +29,34 @@ export const Credentials = () => {
         </div>
       </div>
 
-      <div className="credentials-services">
-        <div className="credentials-service">
-          <DataHub />
-        </div>
+      <Tabs
+        className="tabs"
+        selectedIndex={activeTab}
+        selectedTabClassName="active"
+        onSelect={(index) => setActiveTab(index)}
+      >
+        <TabList className="tab-list">
+          <Tab className="tab">
+            <TbApi />
+            DataHub API Key
+          </Tab>
+          <Tab className="tab">
+            <SiAmazons3 />
+            S3 Token
+          </Tab>
+        </TabList>
 
-        <div className="credentials-service">
-          <S3 />
-        </div>
-      </div>
+        <TabPanel>
+          <div className="tab-content">
+            <DataHub />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="tab-content">
+            <S3 />
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
