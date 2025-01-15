a2407e82145c8894665a9f0e33d0510bc3e860bd
james-hinton <james.hinton@live.co.uk>
2025-01-15 15:18:24 +0000

    feat: update S3Browser to active workspace for dynamic store retrieval

--- src/pages/Stores/components/S3Browser/S3Browser.tsx
+++ src/pages/Stores/components/S3Browser/S3Browser.tsx
@@ -1,47 +1,53 @@
 import { createStorageBrowser, elementsDefault } from '@aws-amplify/ui-react-storage/browser';
+
 import '@aws-amplify/ui-react-storage/storage-browser-styles.css';
 import './S3Browser.scss';
+import { Workspace } from '@/context/WorkspaceContext/types';
+import { useWorkspace } from '@/hooks/useWorkspace';
 
-const { StorageBrowser } = createStorageBrowser({
-  elements: elementsDefault,
+const createStorageBrowserWithWorkspace = (activeWorkspace: Workspace) =>
+  createStorageBrowser({
+    elements: elementsDefault,
 
-  config: {
-    listLocations: async () => {
-      const resp = await fetch('/api/accounts');
-      const data = await resp.json();
+    config: {
+      listLocations: async () => {
+        console.log('activeWorkspace', activeWorkspace);
 
-      return {
-        locations: data.data.accounts[0].workspaces.map((workspace) => ({
-          bucketName: workspace.stores[0].object[0].name,
-          key: workspace.stores[0].object[0].path,
-          region: 'eu-west-2',
-          type: 'PREFIX',
-          permission: 'READWRITE',
-          scope: `s3://${workspace.stores[0].object[0].name}/${workspace.stores[0].object[0].path}/`,
-        })),
-      };
+        return {
+          locations: activeWorkspace.stores.map((store) => ({
+            bucketName: store.object[0].name,
+            key: store.object[0].path,
+            region: 'eu-west-2',
+            type: 'PREFIX',
+            permission: 'READWRITE',
+            scope: `s3://${store.object[0].name}/${store.object[0].path}/`,
+          })),
+        };
+      },
+
+      getLocationCredentials: async () => {
+        const resp = await fetch('/api/workspaces/s3/credentials');
+        const data = await resp.json();
+
+        return {
+          credentials: {
+            accessKeyId: data.accessKeyId,
+            secretAccessKey: data.secretAccessKey,
+            sessionToken: data.sessionToken,
+            expiration: new Date(data.expiration),
+          },
+        };
+      },
+
+      region: 'eu-west-2',
+
+      registerAuthListener: () => {},
     },
-
-    getLocationCredentials: async () => {
-      const resp = await fetch('/api/workspaces/s3/credentials');
-      const data = await resp.json();
-
-      return {
-        credentials: {
-          accessKeyId: data.accessKeyId,
-          secretAccessKey: data.secretAccessKey,
-          sessionToken: data.sessionToken,
-          expiration: new Date(data.expiration),
-        },
-      };
-    },
-
-    region: 'eu-west-2',
-
-    registerAuthListener: () => {},
-  },
-});
+  });
 
 export default function S3Browser() {
+  const { activeWorkspace } = useWorkspace();
+  const { StorageBrowser } = createStorageBrowserWithWorkspace(activeWorkspace);
+
   return <StorageBrowser />;
 }
