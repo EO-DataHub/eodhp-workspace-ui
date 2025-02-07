82eb749ae5355829fa5b128bdfac0035a3094dc8
hcollingwood <hannah.collingwood@telespazio.com>
2025-02-07 09:14:18 +0000

    EODHP-1117 - test1

--- src/pages/Stores/Stores.tsx
+++ src/pages/Stores/Stores.tsx
@@ -3,18 +3,18 @@ import { useState } from 'react';
 
 import { CiCloudOn } from 'react-icons/ci';
 import { FiBox } from 'react-icons/fi';
-import { LuCopyPlus } from 'react-icons/lu';
+// import { LuCopyPlus } from 'react-icons/lu';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import cloudIcon from '@/assets/icons/cloud.svg';
-import { Button } from '@/components/Button/Button';
+// import { Button } from '@/components/Button/Button';
 import { useWorkspace } from '@/hooks/useWorkspace';
 
 import BlockStore from './components/BlockStore/BlockStore';
 
 export const Stores = () => {
   const [activeTab, setActiveTab] = useState(0);
-  const { activeWorkspace } = useWorkspace();
+//   const { activeWorkspace } = useWorkspace();
 
   const renderTabDescription = () => {
     if (activeTab === 1) {
