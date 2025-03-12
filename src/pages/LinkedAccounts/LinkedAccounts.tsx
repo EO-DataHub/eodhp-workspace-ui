f28a9c34ea322ab87c351540001bc931d4f4ac56
Ubuntu <ubuntu@ip-10-2-255-14.eu-west-2.compute.internal>
2025-03-12 16:48:23 +0000

    Refactor to make it more modular.

--- src/pages/LinkedAccounts/LinkedAccounts.tsx
+++ src/pages/LinkedAccounts/LinkedAccounts.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 
 import link from '@/assets/icons/link.svg';
 import { Button } from '@/components/Button/Button';
 
+type LinkableAccount = {
+  internalName: string;
+  externalName: string;
+};
+
+const linkableAccounts: LinkableAccount[] = [
+  {
+    internalName: 'airbus',
+    externalName: 'Airbus',
+  },
+
+  {
+    internalName: 'planet',
+    externalName: 'Planet',
+  },
+];
+
 const LinkedAccounts = () => {
+  const [keys, setKeys] = useState<{ [key: string]: string }>(() => {
+    const initial = {};
+    linkableAccounts.forEach((account) => {
+      initial[account.internalName] = '';
+    });
+    return initial;
+  });
+
   const renderHeader = () => {
     return (
       <div className="header">
@@ -22,32 +47,37 @@ const LinkedAccounts = () => {
     );
   };
 
-  const renderPlanet = () => {
-    return (
-      <div className="linked-accounts__account">
-        <div className="linked-accounts__account-header">
-          Planet <span>| lorem ipsum</span>
-        </div>
-        <div className="linked-accounts__account-input">
-          <input placeholder="Enter your contact / api key" />
-          <span>Message</span>
-        </div>
-        <Button onClick={() => console.log('Clicked')}>Link Account</Button>
-      </div>
-    );
+  const validate = (link: string) => {
+    const key = keys[link];
+    if (!key) {
+      return;
+    }
   };
 
-  const renderAirbus = () => {
+  const renderAccount = (account: LinkableAccount) => {
     return (
       <div className="linked-accounts__account">
         <div className="linked-accounts__account-header">
-          Airbus <span>| lorem ipsum</span>
+          {account.externalName} <span>| lorem ipsum</span>
         </div>
         <div className="linked-accounts__account-input">
-          <input placeholder="Enter your contact / api key" />
+          <input
+            placeholder="Enter your contact / api key"
+            value={keys[account.internalName]}
+            onChange={(e) => {
+              const copy = { ...keys };
+              copy[account.internalName] = e.target.value;
+              setKeys(copy);
+            }}
+          />
           <span>Message</span>
         </div>
-        <Button onClick={() => console.log('Clicked')}>Link Account</Button>
+        <Button
+          disabled={!keys[account.internalName]}
+          onClick={() => validate(account.internalName)}
+        >
+          Link Account
+        </Button>
       </div>
     );
   };
@@ -56,8 +86,7 @@ const LinkedAccounts = () => {
     <div className="content-page">
       {renderHeader()}
       <div className="linked-accounts">
-        {renderPlanet()}
-        {renderAirbus()}
+        {linkableAccounts.map((account) => renderAccount(account))}
       </div>
     </div>
   );
