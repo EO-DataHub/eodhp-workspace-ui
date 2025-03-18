269e11b093c54b4efdb0002d3507a6e570299be5
IsaacJenkinsTPZ <isaac.jenkins@telespazio.com>
2025-03-18 11:33:18 +0000

    Now using Table component for API keys. (#24)
    Co-authored-by: Ubuntu <ubuntu@ip-10-2-255-14.eu-west-2.compute.internal>

---
+++ src/pages/DataHub/placeholder.ts
@@ -0,0 +1,12 @@
+export const tokensPlaceholder: DataHubToken[] = [
+  {
+    created: new Date().toDateString(),
+    expiry: new Date().toDateString(),
+    id: '1234567897654',
+    name: 'Token 1',
+    scope: 'offline_access workspace:test-workspace',
+    token: '',
+    user_id: '1',
+    workspace: 'boeing',
+  },
+];
