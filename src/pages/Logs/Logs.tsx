08fbcf174f9c245128d284e66e214e7a50ca22ad
Isaac Jenkins <isaac.jenkins@telespazio.com>
2025-04-24 14:14:08 +0000

    Initial logs template.

---
+++ src/pages/Logs/Logs.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+import { useWorkspace } from '@/hooks/useWorkspace';
+
+const Logs = () => {
+  const { activeWorkspace } = useWorkspace();
+
+  //   const [logs, setLogs] = useState<string[]>([]);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const getLogs = async () => {
+      const res = await fetch(`/workspaces/${activeWorkspace.name}/harvested_logs`, {
+        method: 'POST',
+      });
+
+      if (!res.ok) {
+        setError('Failed to get Logs');
+        return;
+      }
+
+      const json = await res.json();
+      console.log(json);
+    };
+    getLogs();
+  });
+
+  return (
+    <div className="content-page stores">
+      <div className="header">
+        <div className="header-left">
+          <h2>Logs</h2>
+        </div>
+      </div>
+      {error}
+    </div>
+  );
+};
+
+export default Logs;
