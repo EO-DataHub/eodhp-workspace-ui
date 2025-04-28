afe3291623ab26f8264ea892c1dc3cf3461d6375
Isaac Jenkins <isaac.jenkins@telespazio.com>
2025-04-28 13:30:22 +0000

    Added logs component.

--- src/pages/Logs/Logs.tsx
+++ src/pages/Logs/Logs.tsx
@@ -1,12 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
+import './styles.scss';
 
+import { LazyLog } from 'react-lazylog';
+
+import Sort from '@/assets/icons/sort.svg';
 import { useWorkspace } from '@/hooks/useWorkspace';
+
+import { logsPlaceholder } from './logsPlaceholder';
+import { Log, LogResponse } from './types';
 
 const Logs = () => {
   const { activeWorkspace } = useWorkspace();
 
-  //   const [logs, setLogs] = useState<string[]>([]);
+  const [logs, setLogs] = useState<Log[]>([]);
   const [error, setError] = useState<string>();
+  const [ascend, setAscend] = useState<boolean>(true);
 
   useEffect(() => {
     const getLogs = async () => {
@@ -19,20 +30,86 @@ const Logs = () => {
         return;
       }
 
-      const json = await res.json();
-      console.log(json);
-    };
-    getLogs();
-  });
+      const json: LogResponse = await res.json();
 
-  return (
-    <div className="content-page stores">
+      setLogs(json.messages);
+    };
+
+    if (import.meta.env.VITE_WORKSPACE_LOCAL) {
+      setLogs(logsPlaceholder.messages);
+      return;
+    }
+
+    getLogs();
+  }, [activeWorkspace.name]);
+
+  const renderHeader = () => {
+    return (
       <div className="header">
         <div className="header-left">
           <h2>Logs</h2>
         </div>
       </div>
-      {error}
+    );
+  };
+
+  const renderError = () => {
+    return <div className="logs__error">{error}</div>;
+  };
+
+  const renderButtons = () => {
+    return (
+      <div className="logs__buttons">
+        <img
+          className="logs__buttons-sort"
+          src={Sort}
+          onClick={() => {
+            setAscend(!ascend);
+          }}
+        />
+      </div>
+    );
+  };
+
+  const renderLogs = () => {
+    return (
+      <LazyLog
+        ref={{
+          current: '[Circular]',
+        }}
+        enableSearch
+        follow
+        selectableLines
+        extraLines={1}
+        height="300"
+        overscanRowCount={100}
+        text={getText()}
+      />
+    );
+  };
+
+  const getText = () => {
+    let text;
+
+    if (ascend) {
+      text = logs.map((log) => {
+        return `[${log.datetime}] ${log.message}`;
+      });
+    } else {
+      text = logs.reverse().map((log) => {
+        return `[${log.datetime}] ${log.message}`;
+      });
+    }
+    text = text.join('\n');
+    return text;
+  };
+
+  return (
+    <div className="content-page logs">
+      {renderHeader()}
+      {error && renderError()}
+      {renderButtons()}
+      {renderLogs()}
     </div>
   );
 };
