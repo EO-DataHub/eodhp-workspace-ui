ab7b0a6140267266ee4e1e95bbbdd280eee5a668
Isaac Jenkins <isaac.jenkins@telespazio.com>
2025-04-29 14:40:04 +0000

    Added invoices in navigation.

--- src/components/WorkspaceMenu/navigation.tsx
+++ src/components/WorkspaceMenu/navigation.tsx
@@ -1,4 +1,5 @@
 import credentialsIcon from '@/assets/icons/credentials.svg';
+import invoicesIcon from '@/assets/icons/invoices.svg';
 import linkedAccountsIcon from '@/assets/icons/linked-accounts.svg';
 import logsIcon from '@/assets/icons/logs.svg';
 import membersIcon from '@/assets/icons/members.svg';
@@ -6,6 +7,7 @@ import storesIcon from '@/assets/icons/stores.svg';
 import uploadIcon from '@/assets/icons/upload.svg';
 import { Credentials } from '@/pages/Credentials/Credentials';
 import DataLoader from '@/pages/DataLoader/DataLoader';
+import Invoices from '@/pages/Invoices/Invoices';
 import LinkedAccounts from '@/pages/LinkedAccounts/LinkedAccounts';
 import Logs from '@/pages/Logs/Logs';
 import Members from '@/pages/Members/Members';
@@ -22,7 +24,7 @@ export const navItems: NavItem[] = [
   { label: 'Members', icon: membersIcon, content: <Members /> },
   { label: 'Data Loader', icon: uploadIcon, content: <DataLoader /> },
   { label: 'Linked accounts', icon: linkedAccountsIcon, content: <LinkedAccounts /> },
-  // { label: 'Invoices', icon: invoicesIcon, content: <Invoices /> },
+  { label: 'Invoices', icon: invoicesIcon, content: <Invoices /> },
   { label: 'Credentials', icon: credentialsIcon, content: <Credentials /> },
   {
     label: 'Stores',
