7b1aaa27667a57bda13fee6f6590b1f331f40fa1
tjellicoe <thomas.jellicoe@telespazio.com>
2025-05-07 11:45:00 +0100

    Wrap keys and tokens and add copy buttons for individual and group copying

--- src/pages/S3/S3.tsx
+++ src/pages/S3/S3.tsx
@@ -10,6 +10,23 @@ export const S3 = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  const copyIndividualKey = async (inputString: string) => {
+    await navigator.clipboard.writeText(inputString);
+  };
+
+  const copyAllKeys = async (
+    access_key: string,
+    secret_access_key: string,
+    session_token: string,
+  ) => {
+    const credentials: string =
+      `ACCESS_KEY_ID=${access_key}\n` +
+      `SECRET_ACCESS_KEY=${secret_access_key}\n` +
+      `SESSION_TOKEN=${session_token}`;
+
+    await navigator.clipboard.writeText(credentials);
+  };
+
   const handleCreateToken = async () => {
     try {
       setLoading(true);
@@ -31,14 +48,52 @@ export const S3 = () => {
 
       {newTokenValue ? (
         <div className="new-token-message">
+          <Button
+            onClick={() =>
+              copyAllKeys(
+                newTokenValue.accessKeyId,
+                newTokenValue.secretAccessKey,
+                newTokenValue.sessionToken,
+              )
+            }
+          >
+            Copy credentials to .env file
+          </Button>
           <p>
-            <strong>Access Key ID:</strong> {newTokenValue.accessKeyId}
+            <strong>Access Key ID:</strong>
+            <span style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
+              {newTokenValue.accessKeyId}
+            </span>
+            <Button
+              style={{ display: 'inline-flex', marginLeft: '10px' }}
+              onClick={() => copyIndividualKey(newTokenValue.accessKeyId)}
+            >
+              ⧉
+            </Button>
           </p>
           <p>
-            <strong>Secret Access Key:</strong> {newTokenValue.secretAccessKey}
+            <strong>Secret Access Key:</strong>
+            <span style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
+              {newTokenValue.secretAccessKey}
+            </span>
+            <Button
+              style={{ display: 'inline-flex', marginLeft: '10px' }}
+              onClick={() => copyIndividualKey(newTokenValue.secretAccessKey)}
+            >
+              ⧉
+            </Button>
           </p>
           <p>
-            <strong>Session Token:</strong> {newTokenValue.sessionToken}
+            <strong>Session Token:</strong>{' '}
+            <span style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
+              {newTokenValue.sessionToken}
+            </span>
+            <Button
+              style={{ display: 'inline-flex', marginLeft: '10px' }}
+              onClick={() => copyIndividualKey(newTokenValue.sessionToken)}
+            >
+              ⧉
+            </Button>
           </p>
           <p>
             <strong>Expiration:</strong> {newTokenValue.expiration}
